f007a1cc624c54afb8da10cb3658134f0ba75c51
Kama Corey McKenzie <kcm@Kamas-MBP.attlocal.net>
2023-04-13 12:56:15 -0700

    Added questions page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,39 @@
 
 let showQuestionPage = () => {
   hideHomepage();
-  let quest = document.getElementById("question");
-  if(quest.style.display === "none") {
-    quest.style.display = "block";
-  }
+  document.querySelector("#question").removeAttribute("class", "hide");
 }
 
 let hideHomepage = () => {
-  let home = document.getElementById("home");
-  if(home.style.display !== "none") {
-    home.style.display = "none";
-  }
+  document.querySelector("#home").setAttribute("class", "hide");
 }
 // Create a listener for when the start button is clicked
 let startQuiz = document.getElementById("startQuiz").addEventListener("click", showQuestionPage);
 
-// Default to question page not being displayed
-document.getElementById("question").style.display = "none";
-//document.getElementById("home").style.display = "block";
+
+// Create an array of questions
+let questions = [];
+let question = {"Question": "What does HTML stand for?",
+                "Answer1": "HyperText Markup Language",
+                "Answer2": "HyperTag Markup Language",
+                "Answer3": "HyperText Markdown Language",
+                "Answer4": "HyperTag Markdown Language",
+                "Correct": "1"}
+questions.push(question);
+question = {"Question": "What does CSS stand for?",
+                "Answer1": "Computed Style Sheets",
+                "Answer2": "Console Style Sheets",
+                "Answer3": "Computer Style Sheets",
+                "Answer4": "Cascading Style Sheets",
+                "Correct": "4"}
+questions.push(question);
+
+let loadQuestion = (question, answer1, answer2, answer3, answer4, correct) => {
+  document.querySelector(".displayQuestion").innerHTML = question;
+  document.querySelector(".answer1").innerHTML = answer1;
+  document.querySelector(".answer2").innerHTML = answer2;
+  document.querySelector(".answer3").innerHTML = answer3;
+  document.querySelector(".answer4").innerHTML = answer4;
+}
+
+loadQuestion(questions[0]["Question"], questions[0]["Answer1"], questions[0]["Answer2"], questions[0]["Answer3"], questions[0]["Answer4"])
