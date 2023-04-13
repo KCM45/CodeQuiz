0b01db7bed87cc21bb35373567aa544ffd4705b8
Kama Corey McKenzie <kcm@Kamas-MBP.attlocal.net>
2023-04-13 10:38:36 -0700

    Updated start button to hide homepage and show question page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
-// When start button on home page is clicked, hide homepage and display question page
 
-// Create a listener for when the start button is clicked
-document.getElementById("startQuiz").addEventListener("click", showQuestionPage);
-
-showQuestionPage = () => {
-  console.log("showQ")
+let showQuestionPage = () => {
   hideHomepage();
-  let quest = document.getElementById("question")
+  let quest = document.getElementById("question");
   if(quest.style.display === "none") {
-    console.log("quest.style")
     quest.style.display = "block";
   }
 }
 
-hideHomepage = () => {
+let hideHomepage = () => {
   let home = document.getElementById("home");
-  if(home.style.display === "block") {
+  if(home.style.display !== "none") {
     home.style.display = "none";
   }
 }
+// Create a listener for when the start button is clicked
+let startQuiz = document.getElementById("startQuiz").addEventListener("click", showQuestionPage);
+
+// Default to question page not being displayed
+document.getElementById("question").style.display = "none";
+//document.getElementById("home").style.display = "block";
