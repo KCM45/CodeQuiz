eb2a1222895b60e466955b148f8533834e09b836
Kama Corey McKenzie <kcm@Kamas-MBP.attlocal.net>
2023-04-14 06:42:39 -0700

    Began work on timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,47 @@
-
-let showQuestionPage = () => {
-  hideHomepage();
-  document.querySelector("#question").removeAttribute("class", "hide");
-}
-
-let hideHomepage = () => {
-  document.querySelector("#home").setAttribute("class", "hide");
-}
-// Create a listener for when the start button is clicked
-let startQuiz = document.getElementById("startQuiz").addEventListener("click", showQuestionPage);
-
+let time = 100;
+let displayTime = document.querySelector("#displayTime");
+let interval;
 
 // Create an array of questions
-let questions = [];
-let question = {"Question": "What does HTML stand for?",
-                "Answer1": "HyperText Markup Language",
-                "Answer2": "HyperTag Markup Language",
-                "Answer3": "HyperText Markdown Language",
-                "Answer4": "HyperTag Markdown Language",
-                "Correct": "1"}
+let questions = [
+  {
+    question: "What does HTML stand for?",
+    options: ["HyperText Markup Language", "HyperTag Markup Language", "HyperText Markdown Language","HyperText Markdown Language"],
+    answer: "HyperText Markdown Language"
+  // },
+  // {
+  //   question: ,
+  //   options: [],
+  //   answer: 
+  // },
+  // {
+  //   question: ,
+  //   options: [],
+  //   answer: 
+  //   },
+  // {
+  //   question: ,
+  //   options: [],
+  //   answer: 
+      }
+];
+let question = {
+  "Question": "What does HTML stand for?",
+  "Answer1": "HyperText Markup Language",
+  "Answer2": "HyperTag Markup Language",
+  "Answer3": "HyperText Markdown Language",
+  "Answer4": "HyperTag Markdown Language",
+  "Answer": "HyperText Markup Language"
+}
 questions.push(question);
-question = {"Question": "What does CSS stand for?",
-                "Answer1": "Computed Style Sheets",
-                "Answer2": "Console Style Sheets",
-                "Answer3": "Computer Style Sheets",
-                "Answer4": "Cascading Style Sheets",
-                "Correct": "4"}
+question = {
+  "Question": "What does CSS stand for?",
+  "Answer1": "Computed Style Sheets",
+  "Answer2": "Console Style Sheets",
+  "Answer3": "Computer Style Sheets",
+  "Answer4": "Cascading Style Sheets",
+  "Correct": "4"
+}
 questions.push(question);
 
 let loadQuestion = (question, answer1, answer2, answer3, answer4, correct) => {
@@ -35,5 +51,29 @@ let loadQuestion = (question, answer1, answer2, answer3, answer4, correct) => {
   document.querySelector(".answer3").innerHTML = answer3;
   document.querySelector(".answer4").innerHTML = answer4;
 }
+
+
+let startQuiz = () => {
+  document.querySelector("#home").setAttribute("class", "hide");
+  document.querySelector("#question").removeAttribute("class", "hide");
+  interval = setInterval(myTimer, 1000);
+  displayTime.textContent = time;
+
+}
+
+function myTimer() {
+  time--;
+  displayTime.textContent = time;
+  if (time === 0) {
+    // Create end game function
+    console.log("create endGame()");
+  }
+}
+
+// Create a listener for when the start button is clicked
+document.getElementById("startQuiz").addEventListener("click", startQuiz);
+
+
+
 
 loadQuestion(questions[0]["Question"], questions[0]["Answer1"], questions[0]["Answer2"], questions[0]["Answer3"], questions[0]["Answer4"])
